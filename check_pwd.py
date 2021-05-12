fe99b493a1f55b711da1630f41be17374563503d
Andre O <59809568+forced-badname@users.noreply.github.com>
2021-05-11 21:21:37 -0700

    added requirements

--- check_pwd.py
+++ check_pwd.py
@@ -1,2 +1,11 @@
+# Andre Ortega
+# TDD
+
+# check_pwd accepts a string and returns True if it meets the following criteria:
+#   Must be between 8 and 20 characters (inclusive)
+#   Must contain at least one lowercase letter
+#   Must contain at least one uppercase letter
+#   Must contain at least one digit
+#   Must contain at least on symbol from    ~`!@#$%^&*()_+-=
 def check_pwd(string):
     pass
