22793fa611b768a598b05152dae82a7aa2fe53ce
Andre O <59809568+forced-badname@users.noreply.github.com>
2021-05-11 21:43:13 -0700

    Failed 3rd Test

--- tests.py
+++ tests.py
@@ -5,13 +5,16 @@ from check_pwd import check_pwd
 class TestCase(unittest.TestCase):
 
     def test1(self):
-        input = 'aaaaaaa' #length 7
+        input = 'abcdefg' #length 7
         self.assertEqual(check_pwd(input), False)
 
     def test2(self):
-        input = 'aaaaaaaa' #length 8
+        input = 'abcdefgh' #length 8
         self.assertEqual(check_pwd(input), True)
 
+    def test3(self):
+        input = 'abcdefghijklmnopqrstuv' #length 22
+        self.assertEqual(check_pwd(input), False)
 
 if __name__ == '__main__':
     unittest.main()
