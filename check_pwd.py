da2255d2ba46f3cc19dfbd9c99002c85d49e7def
Andre O <59809568+forced-badname@users.noreply.github.com>
2021-05-11 21:59:46 -0700

    Passed test 4

--- check_pwd.py
+++ check_pwd.py
@@ -11,4 +11,15 @@
 def check_pwd(string):
     if len(string) < 8 or len(string) > 20:
         return False
+
+    bit1 = 0
+    bit2 = 0
+    for element in string:
+        if element.isupper():
+            bit1 = 1
+        if element.islower():
+            bit2 = 1
+    if bit1 == 0 or bit2 == 0:
+        return False
+
     return True
